ab457b1f51b59cfa86c2ef11047e2bc5bff6a72b
Carmen Smith <cheylaracarm77@gmail.com>
2023-08-11 21:28:49 -0500

    created index.html and game.py

--- game.py
+++ game.py
@@ -1,14 +1,6 @@
-#supernatural madlibs
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-noun = input("Enter a noun: ")
-noun2 = input("Enter a noun: ")
-nouns3 = input("Enter a plural noun: ")
-adjective = input("Enter a adjective: ")
-adjective2 = input("Enter a adjective: ")
-place = input("Enter a place: ")
-verb = input("Enter a verb: ")
-number = input("Enter a number: ")
-color = input("Enter a color: ")
-sound = input("Enter a sound: ")
-
-print(f"It is midnight and there is a {color} cat walking across the {place}. There was a {adjective} vampire wearing a {noun}. A {sound} came from the werewolf hiding in the {noun2}. {number} {adjective2} zombies {verb} through a pile of {nouns3} .")
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ prototype.py
@@ -0,0 +1,14 @@
+#supernatural madlibs
+
+noun = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+nouns3 = input("Enter a plural noun: ")
+adjective = input("Enter a adjective: ")
+adjective2 = input("Enter a adjective: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+number = input("Enter a number: ")
+color = input("Enter a color: ")
+sound = input("Enter a sound: ")
+
+print(f"It is midnight and there is a {color} cat walking across the {place}. There was a {adjective} vampire wearing a {noun}. A {sound} came from the werewolf hiding in the {noun2}. {number} {adjective2} zombies {verb} through a pile of {nouns3} .")
